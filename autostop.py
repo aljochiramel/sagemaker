5caad1850f78750ba1288b79f351369b84938e4f
Aljo Ajith <aljoajith@nbdm11c-113.prevalent.com>
2022-05-17 08:02:58 +0530

    autostop

---
+++ autostop.py
@@ -0,0 +1,12 @@
+import boto3 
+
+def get_notebook_name():
+    log_path = '/opt/ml/metadata/resource-metadata.json'
+    with open(log_path, 'r') as logs:
+        _logs = json.load(logs)
+    return _logs['ResourceName']
+
+client=boto3.client('sagemaker')
+client.stop_notebook_instance(
+        NotebookInstanceName=get_notebook_name()
+    )
